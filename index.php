343509a297119149f1f4e883959104f312b68964
tommybad <tommybad@gmail.com>
2016-10-18 09:45:42 +0700

    create project phpbasic2

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            
+            $array1=['a'=>23,'b'=>34,'c'=>45];
+            
+            print_r($array1);
+            echo '<hr>';
+            
+            echo $array1['c'].'<hr>';
+            
+            foreach ($array1 as $key => $value) {
+            echo $key.':'.$value.'<hr>';
+            }
+            
+            if($aaa==0,$aaa<=10,$aaa++){
+                echo $aaa.'<br>';
+            }
+            
+        ?>
+    </body>
+</html>
